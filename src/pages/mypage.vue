e99ceb6d68c646118599e6c73af18c5a5366f8d4
Kazuya Sakakibara <kazuyasakakibara.dev@gmail.com>
2026-05-21 17:46:50 +0900

    マイページ画面のログアウトボタンにハンドラを設定し忘れていたのを修正

--- src/pages/mypage.vue
+++ src/pages/mypage.vue
@@ -1,6 +1,15 @@
 
 <script setup lang="ts">
 import LessonCalendar from "@/components/LessonCalendar.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+function handleLogout() {
+    // TODO: ログアウト処理を実装
+    router.push("/login");
+}
+
 </script>
 
 <template>
@@ -8,7 +17,7 @@ import LessonCalendar from "@/components/LessonCalendar.vue";
     <h3>予約状況</h3>
     <LessonCalendar />
     <div class="actions">
-        <RouterLink to="/mypage/lesson-calendar" class="router-link-as-button">予約の確認・変更</RouterLink>
-        <button>ログアウトする</button>
+        <RouterLink to="/lesson-calendar" class="router-link-as-button">予約の確認・変更</RouterLink>
+        <button @click="handleLogout">ログアウトする</button>
     </div>
 </template>
